3e0f0656d2101c128eac477a27656d8db0a07a9b
Garren Gotthardt <garren.x.gotthardt@gmail.com>
2017-05-21 22:49:29 -0400

    anagrams solution

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+require "pry"
+
+class Anagram
+  attr_accessor :word, :anagrams
+
+  def initialize(word)
+    @word = word
+    @anagrams = []
+  end
+
+  def match(array)
+    array.each do |word|
+    #binding.pry
+      if word.split("").sort == @word.split("").sort
+        @anagrams << word
+      end
+    end
+    @anagrams
+  end
+end
